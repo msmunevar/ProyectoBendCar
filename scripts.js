7b196c624cffe8fc511fc7868dfe45c13bf226f5
msmunevar <90227752+msmunevar@users.noreply.github.com>
2025-04-05 18:07:31 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,22 +1,15 @@
 const swiper = new Swiper('.swiper', {
-      loop: true,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-      },
-    });
-
-// Lógica del botón
-// Mostrar/Ocultar iframe
-const toggleButton = document.getElementById('toggleView');
-const iframe = document.getElementById('viewer-3d');
-const swiperContainer = document.querySelector('.swiper');
-const viewer3D = document.getElementById('viewer-3d');
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
