4781851b7474f17d821fd713619dc6951b78bc9e
msmunevar <90227752+msmunevar@users.noreply.github.com>
2025-04-05 18:03:01 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -20,34 +20,3 @@ const toggleButton = document.getElementById('toggleView');
 const iframe = document.getElementById('viewer-3d');
 const swiperContainer = document.querySelector('.swiper');
 const viewer3D = document.getElementById('viewer-3d');
-
-toggleButton.addEventListener('click', () => {
-  const isIframeVisible = !iframe.classList.contains('hidden');
-  viewer3D.classList.toggle('hidden');
-  toggleButton.classList.toggle('active');
-
-  if (isIframeVisible) {
-    iframe.classList.add('hidden');
-    swiperContainer.classList.remove('hidden');
-    toggleButton.textContent = 'Ver en 3D';
-    toggleButton.classList.remove('active');
-  } else {
-    iframe.classList.remove('hidden');
-    swiperContainer.classList.add('hidden');
-    toggleButton.textContent = 'Ver Fotos';
-    toggleButton.classList.add('active');
-  }
-
-  // Reiniciar animación correctamente
-  toggleButton.classList.remove('animate');
-
-  // Espera al siguiente frame para volver a agregar la clase
-  requestAnimationFrame(() => {
-    toggleButton.classList.add('animate');
-  });
-  // Animación cuando se hace clic
-  toggleButton.classList.add('animate');
-  setTimeout(() => {
-    toggleButton.classList.remove('animate');
-  }, 500);
-});
